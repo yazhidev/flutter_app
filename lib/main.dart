2482c89ebf5ae204f3dd6831b1fda355857f6dec
taki <zengyz@tuya.com>
2021-07-17 15:24:17 +0800

    feat: commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Paper());
-}
-
-class Paper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: CustomPaint(
-        painter: PaperPainter(),
-      ),
-    );
-  }
-
-}
-
-class PaperPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = new Paint();
-    canvas.drawCircle(Offset(100, 100), 10, paint);
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
-
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
